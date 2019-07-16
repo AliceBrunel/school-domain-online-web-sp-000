d25f482edd2548d949ce6e3b1d218d650cf4fb12
Alice Brunel <alc.brunel@gmail.com>
2019-07-16 15:53:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+  attr_reader :grade
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = roster
+    @roster = {}
+  end
+  
+  #define the roster method to make sure it doesn't point to nil
+  def roster
+    @roster
+  end
+  
+  def add_student(name,grade)
+    @roster = roster
+    @grade = grade
+    @name = name
+    @roster.each do |hash|
+      puts hash
+      binding.pry
+      if hash == grade
+        @roster << name
+      else
+        @roster[grade] = {}
+      end
+    end
+  end
+  
+  
+end
